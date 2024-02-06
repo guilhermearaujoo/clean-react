444084b07709ee1a75c8d710342dc8c4975b530b
guilhermearaujoo <ghma.em1@gmail.com>
2024-02-06 10:04:57 -0300

    test: ensure RemoteAuthentication throws invalid InvalidCredentialError on  401

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,12 +1,22 @@
-import { HttpPostClient, HttpPostParams } from "../protocols/http/http-post-client";
+import {
+  HttpPostClient,
+  HttpPostParams,
+} from "@/data/protocols/http/http-post-client";
+import {
+  HttpResponse,
+  HttpStatusCode,
+} from "@/data/protocols/http/http-response";
 
 export class HttpPostClientSpy implements HttpPostClient {
-    url?: string;
-    body?: object;
+  url?: string;
+  body?: object;
+  response: HttpResponse = {
+    statusCode: HttpStatusCode.noContent,
+  };
 
-    post(params: HttpPostParams): Promise<void> {
-        this.url = params.url;
-        this.body = params.body;
-        return Promise.resolve();
-    }
+  post(params: HttpPostParams): Promise<HttpResponse> {
+    this.url = params.url;
+    this.body = params.body;
+    return Promise.resolve(this.response);
+  }
 }
