715c1205ad8e9a1d9b6bc452f052e5a4e4864304
guilhermearaujoo <ghma.em1@gmail.com>
2024-02-08 21:27:38 -0300

    test: ensure AxiosHttpClient calls axios with correct url

---
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { AxiosHttpClient } from "./axios-http-client";
+import { faker } from "@faker-js/faker";
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe("AcxiosHttpClient", () => {
+  test("should call axios with correct URL", async () => {
+    const url = faker.internet.url();
+    const sut = new AxiosHttpClient();
+    await sut.post({ url });
+    expect(mockedAxios).toHaveBeenCalledWith(url);
+  });
+});
