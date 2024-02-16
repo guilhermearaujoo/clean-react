a9d76298f599d983867f7a54863af618371529e7
guilhermearaujoo <ghma.em1@gmail.com>
2024-02-16 19:10:56 -0300

    feat: hello world!

---
+++ src/presentation/pages/login/login.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Styles from "./login-styles.scss";
+
+const Login: React.FC = () => {
+  return <div className={Styles.login}>Hello, World!</div>;
+};
+
+export default Login;
