1294b6191e9f199815573dd3bf54854db92483c4
guilhermearaujoo <ghma.em1@gmail.com>
2024-03-06 22:04:30 -0300

    refactor: change Login tests to use faker

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -42,19 +42,21 @@ describe('Login Component', () => {
     const { sut, validationSpy } = makeSut()
 
     const emailInput = sut.getByTestId('email') as HTMLInputElement
-    fireEvent.change(emailInput, { target: { value: 'any_email' } })
+    const email = faker.internet.email()
+    fireEvent.change(emailInput, { target: { value: email } })
 
     expect(validationSpy.fieldName).toBe('email')
-    expect(validationSpy.fieldValue).toBe('any_email')
+    expect(validationSpy.fieldValue).toBe(email)
   })
 
   test('Should call Validation with correct password', () => {
     const { sut, validationSpy } = makeSut()
 
     const passwordInput = sut.getByTestId('password') as HTMLInputElement
-    fireEvent.change(passwordInput, { target: { value: 'any_password' } })
+    const password = faker.internet.password()
+    fireEvent.change(passwordInput, { target: { value: password } })
 
     expect(validationSpy.fieldName).toBe('password')
-    expect(validationSpy.fieldValue).toBe('any_password')
+    expect(validationSpy.fieldValue).toBe(password)
   })
 })
